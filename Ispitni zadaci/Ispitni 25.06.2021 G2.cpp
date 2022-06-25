a72bee92146dd6e34683cea54ff199403ddca160
Adi <efendic100@gmail.com>
2022-06-25 13:28:04 +0200

    Refactor 25.06.2021 G2.cpp

--- Ispitni zadaci/Ispitni 25.06.2021 G2.cpp
+++ Ispitni zadaci/Ispitni 25.06.2021 G2.cpp
@@ -110,7 +110,7 @@ public:
 
     void AddElement(T1 el1, T2 el2)
     {
-        if (_trenutno == max)
+        if (*_trenutno == max)
             throw exception("Prekoracen opseg kolekcije!");
 
         if (_dupliranje == Dupliranje::BEZ_DUPLIKATA)
